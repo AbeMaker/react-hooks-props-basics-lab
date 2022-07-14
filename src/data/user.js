0618a8d7e8f5d494e3a994035fdbf0a1a28daf24
Abraham Anyang <anyangmaker90@gmail.com>
2022-07-14 19:55:49 +0300

    tests passed

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
-  bio: "I made this!",
+  name: "Abraham Anyang",
+  city: "Juba",
+  bio: "I am an experienced Electrical and electronics graduate engineer and more importantly, I have vast knowledge in web designing with one year in the tech industry.",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://https://github.com/AbeMaker",
+    linkedin: "https://www.linkedin.com/in/abe-maker-296b0418a/",
   },
 };
 
